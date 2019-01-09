514314f563828079a1d3f94106351e6a3c486851
WestleyR <39201023+WestleyR@users.noreply.github.com>
2019-01-08 21:01:43 -0800

    Update func_info.h

--- func_info.h
+++ func_info.h
@@ -1,1 +1,19 @@
+// created by: WestleyR
+// email: westleyr@nym.hush.com
+// https://github.com/WestleyR/print-line
+// date: Jan 8, 2018
+// version-1.0.0
+//
+// The Clear BSD License
+//
+// Copyright (c) 2019 WestleyR
+// All rights reserved.
+//
+// This software is licensed under a Clear BSD License.
+//
+
 void print_info();
+
+//
+// End func_info.h
+//
